8517f0eb7912d2c2dfc3a4774b090490c77909bb
William Wong <william.wai.lim.wong@gmail.com>
2023-04-30 12:27:37 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
 /* Your Code Here */
+function createEmployeeRecord(employeeArray) {
+    return {
+      firstName: employeeArray[0],
+      familyName: employeeArray[1],
+      title: employeeArray[2],
+      payPerHour: employeeArray[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    }
+  }
 
+  function createEmployeeRecords(arr) {
+    return arr.map(record => createEmployeeRecord(record))
+  }
+
+function createTimeInEvent(date){
+    //const timeStamp = date.split(' ');
+    //const timeInObj = {type : "TimeIn", hour : parseInt(timeStamp[1]), date : timeStamp [0],};
+    this.timeInEvents.push({type : "TimeIn", hour : parseInt(date.slice(-4)), date : date.slice(0,10)});
+    return this;
+}
+
+function createTimeOutEvent(date){
+    const timeStamp = date.split(' ');
+    const timeOutObj = { type : "TimeOut", hour : parseInt(timeStamp[1]), date : timeStamp [0],};
+    this.timeOutEvents.push(timeOutObj);
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(event => event.date === date);
+    const timeOut = this.timeOutEvents.find(event => event.date === date);
+    return (timeOut.hour - timeIn.hour)/100;
+  }
+  
+  function wagesEarnedOnDate(date) {
+    const wage = this.payPerHour
+    const hoursWorked = hoursWorkedOnDate.call(this, date)
+    return wage * hoursWorked;
+  }
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +60,11 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find((record) => record.firstName === firstName)
+}
+
+function calculatePayroll(employeeRec) {
+    const totalForEachEmployee = employeeRec.map(record => allWagesFor.call(record))
+    return totalForEachEmployee.reduce((total, empTotal) => total + empTotal)
+  }
